5bb47faef7e1f1256daf43095e4e4a72cb345256
Erick Medeiros <erick_mmedeiros@hotmail.com>
2019-05-14 16:32:24 -0300

    add: aula de variaveis

---
+++ exercicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,21 @@
+var a = 3;
+let b = 4;
+
+var a = 30;
+b = 40;
+
+console.log(a,b);
+
+a = 300;
+b = 400;
+
+console.log(a,b);
+
+const c = 5;
+// c = 50
+console.log(c);
+
+/* cosnt de constante
+var de variavel obvio e ela nao pode ser redeclarada (via linha 1-4)
+let forma de decalracao de variavel utilizada
+*/
