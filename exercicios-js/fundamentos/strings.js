3c8e36a68c7b68408b3d9f7fef76d4edf4993a42
Erick Medeiros <erick_mmedeiros@hotmail.com>
2019-05-21 23:11:42 -0300

    test: ssh key 1

--- exercicios-js/fundamentos/strings.js
+++ exercicios-js/fundamentos/strings.js
@@ -9,4 +9,4 @@ console.log(escola.substring(1))
 console.log(escola.substring(0, 3))
 
 console.log('Escola '.concat(escola).concat("!"))
-console.log(escola.replace(3, 'e'))
+console.log(escola.replace(3, 'e')) //aula de strings
