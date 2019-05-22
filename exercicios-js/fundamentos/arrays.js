0e1c73ea1381fd21ccf480c368d7e34d47848ad2
Erick Medeiros <erick_mmedeiros@hotmail.com>
2019-05-22 16:43:35 -0300

    add: aula array (pop e del)

---
+++ exercicios-js/fundamentos/arrays.js
@@ -0,0 +1,7 @@
+const valores = [7.2, 9.2]
+console.log(valores)
+
+console.log(valores.length) //tamanho do array
+console.log(valores.pop()) // retira o ultimo valor e joga no coosole
+delete valores[0] //deleta o valor especificado
+console.log(valores)
